3fec2ad0c10ea6509cfd65b37e753b46149cc542
Johnny <Pereirajoaovitor85@gmail.com>
2023-09-15 12:36:54 -0300

    primary

---
+++ music_app/lib/core/app/music_app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MusicAppColors {
+  MusicAppColors._();
+
+  // Black (preto)
+  static Color get primaryColor => Color(0xff000000);
+
+  //White (branco)
+  static Color get secondaryColor => Color(0xffffffff);
+
+  // Green (verder)
+  static Color get tertiaryColor => Color(0xff779933);
+
+  // Red (vermelho)
+  static Color get errorColor => Color(0xffdc3545);
+}
